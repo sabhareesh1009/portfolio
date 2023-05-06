5e959fd0bf238de35726fdb308a6c33161d316eb
sabhareesh.kandikonda <sabhareeshkandikonda1109@gmail.com>
2023-05-07 03:22:16 +0530

    designed the home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { TwitterIcon, GithubIcon, LinkedInIcon } from "./Icons";
 import Logo from "./Logo";
+import { motion } from "framer-motion";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
   return (
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={`${className} relative group`}>
       {title}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+        ${router.asPath === href ? "w-full" : "w-0"}
+        `}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
@@ -19,23 +30,30 @@ const NavBar = () => {
         <CustomLink href="/projects" title="Projects" className="mx-4" />
         <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
-      =
-      <nav>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
+      <nav className="flex items-center justify-center fle-wrap">
+        <motion.a
+          href="https://twitter.com"
+          target="_blank"
+          className="w-6 mr-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a href="https://twitter.com"
+          target="_blank"
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}>
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a href="https://twitter.com"
+          target="_blank"
+          className="w-6 ml-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}>
+          <GithubIcon />
+        </motion.a>
       </nav>
       <div className="absolute left-[50%] top-2 transalate-x-[-50%]">
         <Logo />
