4c225bdd01388c08e35d972ebd4fd910f56d0760
sabhareesh.kandikonda <sabhareeshkandikonda1109@gmail.com>
2023-05-11 01:59:03 +0530

    Designed the articles pages.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,9 +9,9 @@ import projectImage1 from "../../public/images/projects/crypto-screener-cover-im
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between relative rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
-
-<div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark" />
+    <article className="relative w-full flex items-center justify-between rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      {/* <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" /> */}
+      <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
       <Link
         href={link}
         tabIndex="_blank"
@@ -47,9 +47,10 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   );
 };
 
-const Project = ({ type, title, summary, img, link, github }) => {
+const Project = ({ title, img, link, github }) => {
   return (
     <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+       <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
       <Link
         href={link}
         tabIndex="_blank"
@@ -96,7 +97,7 @@ const projects = () => {
             className="mb-16"
           />
 
-          <div className="grid grid-cols-12 gap-24">
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 title="Crypto Screener Application"
@@ -109,11 +110,7 @@ const projects = () => {
             <div className="col-span-6">
               <Project
                 title="Crypto Screener Application"
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency."
                 link={"/"}
-                type="Featured Project"
                 img={projectImage1}
                 github="/"
               />
@@ -121,11 +118,7 @@ local currency."
             <div className="col-span-6">
               <Project
                 title="Crypto Screener Application"
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency."
                 link={"/"}
-                type="Featured Project"
                 img={projectImage1}
                 github="/"
               />
@@ -143,11 +136,7 @@ local currency."
             <div className="col-span-6">
               <Project
                 title="Crypto Screener Application"
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency."
                 link={"/"}
-                type="Featured Project"
                 img={projectImage1}
                 github="/"
               />
@@ -155,11 +144,7 @@ local currency."
             <div className="col-span-6">
               <Project
                 title="Crypto Screener Application"
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency."
                 link={"/"}
-                type="Featured Project"
                 img={projectImage1}
                 github="/"
               />
