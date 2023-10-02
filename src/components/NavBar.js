f5b990f0947476c5f03aa6d1318b1bdf1afe23fc
sabhareesh.kandikonda <sabhareeshkandikonda1109@gmail.com>
2023-10-02 16:17:02 +0530

    fixed the hover issue in navbar items

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,7 +44,7 @@ const CustomMobileLink = ({ href, title, className = "", toggle }) => {
       <span
         className={`h-[1px] inline-block bg-light dark:bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
         ${router.asPath === href ? "w-full" : "w-0"}
-        dark:bg-light`}
+        `}
       >
         &nbsp;
       </span>
